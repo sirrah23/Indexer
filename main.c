19f7014c304c57e04abb97b352ea217bb7586c10
vonquilon <von.quilon@gmail.com>
2013-10-12 01:57:25 -0400

    added readFile function - it's unfinished

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include "index.h"
+
+int compareInts(void *p1, void *p2)
+{
+    int i1 = *(int*)p1;
+    int i2 = *(int*)p2;
+
+    return i1 - i2;
+}
+
+int compareStrings(void *p1, void *p2)
+{
+    char *s1 = p1;
+    char *s2 = p2;
+
+    return strcmp(s2, s1);
+}
+
+int readFile(char *filename, IndexerPtr insertree) {
+    
+}
 
 int main(int argc, char *argv[]) {
     if(argc != 3) {
@@ -8,8 +29,11 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    IndexerPtr indexer = IndexerCreate(&compareStrings, &compareInts);
+
+    char *objName = argv[2];
     struct stat obj;
-    stat(argv[2], &obj);
+    stat(objName, &obj);
     if(S_ISREG(obj.st_mode))
         printf("File is regular.\n");
     else if (S_ISDIR(obj.st_mode))
