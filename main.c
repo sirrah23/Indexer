434bc05b47bbbec29e6ae5fecf6999adde03f4e1
vonquilon <von.quilon@gmail.com>
2013-10-12 14:37:16 -0400

    finished the readFile function, not finished yet though

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include "tokenizer.h"
 #include "index.h"
+
+static char alphaNum[63] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\0";
 
 int compareInts(void *p1, void *p2)
 {
@@ -19,8 +25,31 @@ int compareStrings(void *p1, void *p2)
     return strcmp(s2, s1);
 }
 
+void toLowerCase(char *string) {
+    int i;
+    for(i = 0; string[i] != '\0'; i++)
+        string[i] = tolower(string[i]);
+}
+
 int readFile(char *filename, IndexerPtr insertree) {
-    
+    char buffer[BUFSIZ];
+    FILE *file = fopen(filename, "r");
+    if(!file)
+        return 0;
+
+    while(fgets(buffer, sizeof(buffer), file)) {
+       TokenizerT *tokenizer = TKCreate(alphaNum, buffer);
+       if(tokenizer == NULL)
+           return 0;
+       char *token;
+       while((token = TKGetNextToken(tokenizer)) != NULL) {
+            toLowerCase(token);
+            if(!IndexerInsert(token, filename, insertree)
+                return 0;
+       }
+       TKDestroy(tokenizer);
+    }
+    return 1;
 }
 
 int main(int argc, char *argv[]) {
