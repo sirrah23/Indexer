42e4a77cc7c671a6015dcb5bf338725603f8a255
Harris <harris.seth@rutgers.edu>
2013-10-12 00:12:05 -0400

    Adding initial indexer

---
+++ index.c
@@ -0,0 +1,34 @@
+#include "sorted-list.h"
+#include "tokenizer.h"
+#include "indexer.h"
+
+struct Indexer{
+	SortedListPtr words;
+}
+
+typedef struct Indexer* IndexerPtr;
+
+IndexerPtr IndexerCreate(CompareFuncT cf, CompareFuncT df){
+	IndexerPtr newindexer = malloc(sizeof(struct Indexer));
+	newindexer->words = malloc(sizeof(struct SortedList));
+	newindexer->words = SLCreate(cf);
+	newindexer->words->files = malloc(sizeof(struct SortedList));
+	new->index->words->files = SLCreate(df);
+	return newindexer;
+}
+
+void IndexerDestroy(IndexPtr destroyit){
+	SLDestroy(destroyit->words);
+	free(destroyit);
+}
+
+int IndexerInsert(char* word, char* filename, IndexerPtr insertee)
+{
+	int x = SLInsert(insertee,word);
+	int y = SLInsert(insertee->files,filename);
+	if(x && y){
+		return 1;
+	}
+	return 0;
+}
+
