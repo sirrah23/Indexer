e4f4e9e1718b643e7ca3d2a09077733d37b43321
Harris <harris.seth@rutgers.edu>
2013-10-15 15:44:04 -0400

    Changed index.c so it should work now

--- index.c
+++ index.c
@@ -3,7 +3,7 @@
 IndexerPtr IndexerCreate(CompareFuncT cf, CompareFuncT df){
 	IndexerPtr newindexer = malloc(sizeof(struct Indexer));
 	newindexer->words = SLCreate(cf);
-	newindexer->words->files = SLCreate(df);
+	newindexer->files = SLCreate2(df);
 	return newindexer;
 }
 
@@ -14,9 +14,8 @@ void IndexerDestroy(IndexPtr destroyit){
 
 int IndexerInsert(char* word, char* filename, IndexerPtr insertee)
 {
-	int x = SLInsert(insertee,word); /*this could be wrong*/
-	int y = SLInsert(insertee->files,filename); /*so is this*/
-	if(x && y){
+	int x = SLInsert(insertee->words,word,filename); /*this could be wrong*/
+	if(x){
 		return 1;
 	}
 	return 0;
