0e0cec0609c624097a95c720ef5d222b544b19c7
vonquilon <von.quilon@gmail.com>
2013-11-23 23:21:51 -0500

    Done

--- pa5/database.c
+++ pa5/database.c
@@ -53,3 +53,65 @@ CustomerPtr get(DatabasePtr database, int customer_id) {
     else
         return NULL;
 }
+
+/*
+ * Copies n characters from str2 to str1 starting at position
+ * offset in str2. Returns str1.
+ */
+char *copy(char *str1, char *str2, int offset, int n) {
+    int i;
+    for(i = 0; i < n; i++)
+        str1[i] = str2[i+offset];
+    str1[i] = '\0';
+    return str1;
+}
+
+/*
+ * Inserts a customer's information into the database.
+ * Returns 1 on success and 0 on failure.
+ */
+int insert(DatabasePtr database, char *information) {
+    /*Make a customer node*/
+    CustomerPtr customer;
+    if((customer = makeCustomer()) == NULL)
+        return 0;
+    
+    int id; float money;
+    char *name, *address, *state, *zipcode;
+    char *token;
+
+    /*Stores name in the customer node*/
+    if((token = strtok(information, "|")) != NULL) {
+        name = malloc(sizeof(char) * (strlen(token)-1));
+        customer->name = copy(name, token, 1, strlen(token)-2);
+    }
+    /*Stores customer ID in the customer node*/
+    if((token = strtok(NULL, "|")) != NULL) {
+        if(sscanf(token, "%d", &id) == 1)
+            customer->id = id;
+    }
+    /*Stores money amount in the customer node*/
+    if((token = strtok(NULL, "|")) != NULL) {
+        if(sscanf(token, "%f", &money) == 1)
+            customer->money = money;
+    }
+    /*Stores address in the customer node*/
+    if((token = strtok(NULL, "|")) != NULL) {
+        address = malloc(sizeof(char) * (strlen(token)-1));
+        customer->address = copy(address, token, 1, strlen(token)-2);
+    }
+    /*Stores customer's state in the customer node*/
+    if((token = strtok(NULL, "|")) != NULL) {
+        state = malloc(sizeof(char) * (strlen(token)-1));
+        customer->state = copy(state, token, 1, strlen(token)-2);
+    }
+    /*Stores customer's zipcode in the customer node*/
+    if((token = strtok(NULL, "|")) != NULL) {
+        zipcode = malloc(sizeof(char) * (strlen(token)-1));
+        customer->zipcode = copy(zipcode, token, 1, strlen(token)-2);
+    }
+    
+    /*Stores customer into the database*/
+    database->table[id] = customer;
+    return 1;
+}
--- pa5/database.h
+++ pa5/database.h
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+
 /*
  * A node that holds customer information.
  */
