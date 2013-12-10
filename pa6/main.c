a8a99b010680bb31cc513df9c89af7e5107179fb
vonquilon <von.quilon@gmail.com>
2013-12-09 23:19:25 -0500

    done

--- pa6/main.c
+++ pa6/main.c
@@ -1,66 +1,11 @@
 #include "malloc2.h"
 
 int main() {
-<<<<<<< HEAD
     //test 1
     char *test, *test1;
     printf("----- START TEST1 -----\n");
     test = malloc(300);
     test1 = malloc(4600);
-=======
-    char *test, *test1, *test2, *test3, *test4;
-    /*test1*/
-    test = malloc(300);
-    test1 = malloc(4600);
-    free(test);
-    free(test1);
-
-    /*test2*/
-    free(test);
-    test1 = malloc(5005);
-    free(test1);
-
-    /*test3*/
-    test = malloc(300);
-    test2 = malloc(1000);
-    test3 = malloc(1000);
-    free(test2 + 5);
-    free(test);
-    free(test);
-    free(test2);
-    free(test3);
-
-    /*test4*/
-    test = malloc(200);
-    test2 = malloc(400);
-    test3 = malloc(600);
-    test4 = malloc(800);
-    free(test4);
-    free(test3);
-    free(test2);
-    free(test);
-    test = malloc(200);
-    test2 = malloc(400);
-    test3 = malloc(600);
-    test4 = malloc(800);
-    free(test4);
-    free(test3);
-    free(test2);
-    free(test);
-    test = malloc(200);
-    test2 = malloc(400);
-    test3 = malloc(600);
-    test4 = malloc(800);
-    free(test4);
-    free(test3);
-    free(test2);
-    free(test);
-
-    /*test5*/
-    test = malloc(100);
-    free(test);
-    test = malloc(100);
->>>>>>> 21c8000428bad59e81818ad965433296bdde51be
     free(test);
     free(test1);
     printf("------ END TEST1 ------\n");
