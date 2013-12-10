accfc81790c68aa22aa0cc60ea4bbbbaa44ee5fb
Harris Seth <hns26@top.cs.rutgers.edu>
2013-12-09 22:25:41 -0500

    testcases and readme

--- pa6/main.c
+++ pa6/main.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 int main() {
-    char *test, *test1;
-    test = malloc(2500);
-    test1 = malloc(2536);
+    char *test;
+    test = malloc(100);
     free(test);
-    test = malloc(2100);
-    free(test1);
+    test = malloc(100);
     free(test);
-    free(test);
-    free(test1);
+
+
+
     return 0;
 }
